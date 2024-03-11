58cef6ed558b2b595aafa6aeb93c11e83870424a
matheobz <matheo@berrez.com>
2024-03-11 14:47:12 +0100

    reste css

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,6 @@ export default {
 
 <style>
 @import url('assets/styles/global.css');
-@import url('assets/styles/fenetre.css');
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '~normalize.css/normalize.css';
+
 </style>
